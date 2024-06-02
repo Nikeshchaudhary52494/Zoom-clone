2950aadda0f773d6dbd7f83e0adbdc812d9ef02a
Nikesh Chaudhary <nikeshchaudhary52494@gmail.com>
2024-06-02 20:27:55 +0530

    sidebar added

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "YOOM",
+  description: "Video calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-dark-2`}>
+        {children}
+      </body>
+    </html>
+  );
+}
